cd669a686ccd78331e07024f66d89879f3c9a8f2
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-20 00:54:36 -0300

    Add event listener for adding books, persistence not yet implemented

--- app.js
+++ app.js
@@ -50,5 +50,23 @@ class UI {
 // Store Class: for handling data storage
 // Event: for displaying books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
 // Event: add a book
+document
+.querySelector('#book-form')
+.addEventListener('submit', (e) => {
+    // Prevent actual form submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+
+    // Add book to list
+    UI.addBookToList(book);
+});
 // Event: remove a book
